bed175c37aba4c559de1ef40c89c543b86af2ff5
Vishal <vishalp000111@gmail.com>
2021-07-19 11:49:18 +0530

    recursion iii done

---
+++ 11) Recursion/iii/permutation.cpp
@@ -0,0 +1,25 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+void permutation(string s, string ans)
+{
+
+    if (s.length() == 0)
+    {
+        cout << ans << endl;
+        return;
+    }
+    for (int i = 0; i < s.length(); i++)
+    {
+        char ch = s[i];
+        string rest = s.substr(0, i) + s.substr(i + 1);
+        permutation(rest, ans + ch);
+    }
+}
+
+int main()
+{
+    permutation("ABC", "");
+    return 0;
+}
